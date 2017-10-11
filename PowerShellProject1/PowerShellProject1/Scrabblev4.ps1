ec7527050cbe80c9812ad55035c89342d6009a75
KyleF <kylef@DESKTOP-VBCRQED>
2017-10-11 22:45:42 +1100

    new code added and flows better, issue with the removal of the tile from bag, seems to take too many.

--- PowerShellProject1/PowerShellProject1/Scrabblev4.ps1
+++ PowerShellProject1/PowerShellProject1/Scrabblev4.ps1
@@ -26,26 +26,46 @@ $J=@{HowMany=1;Score=8}
 $X=@{HowMany=1;Score=8}
 $Q=@{HowMany=1;Score=10}
 $Z=@{HowMany=1;Score=10}
-# define Variables
+# defined script level Variables
 $ScrabbleBagTiles = @(65..90) + 94
-$requiredtiles = 7
-# define functions
+$requiredtiles = 1
+$ScrabbleBag = @()
+$tilerack = @()
+# defined functions
 	
-Function pickxtiles{
-	# pick X tiles function
+Function pickatile($CreatedBag){
+	# pick a tiles function
 		# while $scrabblebag count is > 0
-			# foreach $tile in $requiredtiles
-				# get random tile from $scrabblebag
-				# remove the tile that was recieved
-				# convert the tile to a alpha character from numeric
-				# return alpha character to array $tilerack
-			# return the $tilerack
+			# get random tile from $scrabblebag
+			# return tile
+	While($CreatedBag -gt 0){
+		$tile = Get-Random $CreatedBag
+		Return $tile
+	}
 }
-Function createabag {
+function removetilefrombag($pickedtile,$AlteringBag){
+	# remove a tile from bag function
+		# remove the tile that was recieved
+		# return updated bag
+	$AlteringBag = $AlteringBag -ne $pickedtile
+	Return $AlteringBag
+}
+Function createabag($tilesymbol,$tilequantity,$makebag){
 	# create a scrabble bag	function
-	# 
-	
+		# Get a list of the type of tiles that should be in the bag
+		# get a list of how many tiles of each type should be in the bag
+		# add how many of each type to the bag
+		# return the bag
+	Foreach($burnedvar in 1..$tilequantity){
+		$makebag += $tilesymbol
+	}
+	Return $makebag
 }
+
+
+# convert the tile to a alpha character from numeric
+# return alpha character to array $tilerack
+# return the $tilerack
 # display those tiles
 														
 
@@ -56,9 +76,19 @@ Function createabag {
 # start program
 	#remove Scrabble bag start count as its for testing only
 	Write-Output "How many tiles start in bag "$ScrabbleBag.Count 
-createabag $ScrabbleBagTiles
-pickxtiles
-
+# foreach $tile in $requiredtiles
+foreach($maketile in $ScrabbleBagTiles){
+	$convertedtile = [char]$maketile
+	$quanityoftile = (Get-Variable $convertedtile -ValueOnly).HowMany
+	$ScrabbleBag = createabag $maketile $quanityoftile $ScrabbleBag
+}
+#remove Scrabble bag start count as its for testing only 
+Write-Output "How many tiles start in bag "$ScrabbleBag.Count
+Foreach($burnedvar1 in 1..$requiredtiles){
+	$Tilegrab = pickatile $ScrabbleBag
+	$ScrabbleBag = removetilefrombag $Tilegrab $ScrabbleBag
+	$tilerack += $Tilegrab
+}
 
 
 ####################### TEST START #######################
